27f51f255ef1634f796bf8a3e0c3e814403bbb48
AbdullahRahimy <rahimyabdullah9@gmail.com>
2024-11-10 13:12:42 -0500

    YML update

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-         baseUrl: 'https://www.axlehealth.com/',
-         viewportWidth: 1400,
-         viewportHeight: 1200,
-  },
-});
